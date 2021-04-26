60c202d99c139cdc6ca55f292ac779695920e86b
Huangsiyu1918 <64762887+Huangsiyu1918@users.noreply.github.com>
2021-04-26 16:34:38 +0930

    new3

--- query3-4.sql
+++ query3-4.sql
@@ -1,3 +1,4 @@
 SELECT Students.given_name, Students.family_name 
-FROM Students, Subjects
-WHERE Subjects.subject_code = 'COMP SCI 2207';
+FROM Students, Enrolments
+WHERE Enrolments.subject_code = 'COMP SCI 2207'
+AND Students.student_id = Enrolments.student_id;
