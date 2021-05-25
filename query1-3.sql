a1dca8cd17d5c243bac65a12e7bcfcfc601f2843
Huangsiyu1918 <64762887+Huangsiyu1918@users.noreply.github.com>
2021-05-25 21:17:18 +0930

    update

--- query1-3.sql
+++ query1-3.sql
@@ -1,4 +1,4 @@
 SELECT first_name, last_name, rental_date  FROM customer, rental
 WHERE customer.customer_id = rental.customer_id
-ORDER BY rental_date ASC
-LIMIT 1;
+ORDER BY (rental.return_date - rental.rental_date) DESC
+limit 1;
